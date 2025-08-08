617696fb93590bad822c9087177a0ede69f2323e
iniayyu <wahyusetyawardani06@gmail.com>
2025-08-08 15:56:15 +0700

    Add files via upload

---
+++ DefaultArgumentFungsi.py
@@ -0,0 +1,30 @@
+# contoh 1
+def say_hello(nama = "cantik"):
+    print(f"halo {nama}")
+    
+say_hello("ayyu")
+say_hello() 
+
+# contoh 2
+def sapa_dia(nama, pesan = "apa kabar"):
+    print(f"hai {nama}, {pesan}")
+    
+sapa_dia("ayyu", "hai cantikk")
+sapa_dia("ayyu")
+
+#contoh 3
+def hitung_pangkat(angka, pangkat = 2):
+    hasil = angka**pangkat
+    return(hasil) 
+
+print(hitung_pangkat(3,7))
+hasil = hitung_pangkat(pangkat = 8, angka=9)
+print(hasil)
+
+# contoh 4
+def fungsi(input1=1,input2=2,input3=3,input4=4):
+    hasil = input1 + input2 + input3 + input4
+    return hasil
+
+print(fungsi())
+print(fungsi(input3=10))
